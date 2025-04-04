474fc7b28f0f81414832e9a266647852f4918316
Chambha <Chambha@whs.school.nz>
2025-04-04 18:59:05 +1300

    Add files via upload

--- BankingAssessment/Account.java
+++ BankingAssessment/Account.java
@@ -5,6 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.Scanner;
 
 public class Account
 {
@@ -26,5 +27,60 @@ public class Account
     
     public double displayBalance(){  
         return currentBalance;
+    }
+    
+    public void everydayAccount() {
+        this.accountType = "Everyday Account";
+    }
+    
+    public void savingsAccount(){
+        this.accountType = "Savings Account";
+    }
+    
+    public void currentAccount() {
+        this.accountType = "Current Account";
+    }
+    
+    public static Account createAccount(){
+        Scanner keyboard = new Scanner(System.in);
+                
+        System.out.println("Input account number");
+        Account account1 = new Account();  
+        //account1.accountNumber = keyboard.nextLine();
+        String accountNumberInput = keyboard.nextLine(); //User inputs account number
+        
+        System.out.println("Select an option for account type."); //Switch statement to select different account types
+        System.out.println("(1) - Everyday"); 
+        System.out.println("(2) - Savings"); 
+        System.out.println("(3) - Current");
+        
+        if(keyboard.hasNextInt()){
+            int choice = keyboard.nextInt(); //checks if the input is a number
+            keyboard.nextLine(); //clear the enter
+            if (choice != 1 && choice != 2 && choice != 3){
+                System.out.println("Please enter a valid input"); //if the input is not equal to 1, 2, or 3, print out invalid
+            } else {
+                switch (choice){
+                    case 1: //Everyday   
+                        System.out.println("Everyday");
+                        account1.everydayAccount(); //if "Everyday" is selected, account type is set to Everyday
+                        break;
+            
+                    case 2: //Savings
+                        System.out.println("Savings"); 
+                        account1.savingsAccount(); //if "Savings" is selected, account type is set to Savings
+                        break;
+                
+                    case 3: //Current
+                        System.out.println("Current");
+                        account1.currentAccount(); //if "Current" is selected, account type is set to Current
+                        break;
+                }
+            }
+            } else {
+                System.out.println("Input must be a number"); //if the input is not equal to a number print out invalid
+        }
+        account1.accountNumber = accountNumberInput;
+        return account1;  
     }
 }
--- BankingAssessment/main.java
+++ BankingAssessment/main.java
@@ -17,20 +17,107 @@ public class main
         Scanner keyboard = new Scanner(System.in);
         
         ArrayList<Customer> customerList = new ArrayList<Customer>(); //Create arraylist to store customer info during the day
-        customerList.add(createCustomer()); //add new customer to the arraylist
-        //System.out.println(customerList.get(0).Name);          
-        customerList.get(0).addCustomerAccount(createAccount());
-        
-        // System.out.println(customerList.get(0).accountList.get(0).accountType);
-        // System.out.println(customerList.get(0).accountList.get(0).displayBalance());
-        // customerList.get(0).accountList.get(0).Deposit(10.0);
-        // System.out.println(customerList.get(0).accountList.get(0).displayBalance());
-        // customerList.get(0).accountList.get(0).Withdraw(5.0);
-        // System.out.println(customerList.get(0).accountList.get(0).displayBalance());
         
         boolean running = true;
         File myFile = new File("bankData.csv");
         
+        
+        try {
+            Scanner myReader = new Scanner(myFile);
+            ArrayList<String> fileNames = new ArrayList<String>();
+            
+            while(myReader.hasNextLine()){
+                myReader.useDelimiter(","); //seperates the informaion in the file by comma
+                 while (myReader.hasNext()) {
+                    fileNames.add(myReader.next());
+                }
+            }
+
+            //build customer
+            for (int i=0; i<fileNames.size(); i++){
+                //System.out.println(fileNames.get(i));
+            }
+            
+            Customer fileCustomer = new Customer(); 
+            fileCustomer.Name = fileNames.get(0); //gets each element of the arrayList startring from 0
+            fileCustomer.Address = fileNames.get(1);
+            
+            Account fileAccount = new Account();
+            fileAccount.accountNumber = fileNames.get(2);
+            fileAccount.accountType = fileNames.get(3);
+            Double accountBalance = Double.parseDouble(fileNames.get(4)); //converts the string to a double so that the maths in the account class works for the balance
+            fileAccount.currentBalance = accountBalance;
+            
+            fileCustomer.addCustomerAccount(fileAccount);
+            
+            customerList.add(fileCustomer); //add the customer in the file to the customerList arraylist so that any existing customers can have their info updated 
+            
+        } catch(IOException e) {
+            System.out.println(e);
+            System.out.println("There was an error reading from the file");
+        }
+        
+        
+        System.out.println("Welcome! would you like to:"); //prompt user to select a function
+        System.out.println("(1) - Create account for a new customer"); 
+        System.out.println("(2) - Create account for existing customer"); 
+        System.out.println("(3) - Close account for existing customer");
+        System.out.println("(4) - Access balance of an account");
+        System.out.println("(5) - Deposit/withdraw from an account");
+        
+        if(keyboard.hasNextInt()){
+            int selectOption = keyboard.nextInt(); //checks if the input is a number
+            keyboard.nextLine(); //clear the enter
+            if (selectOption != 1 && selectOption != 2 && selectOption != 3 && selectOption != 4 && selectOption != 5){
+                System.out.println("Please enter a valid input"); //if the input is not equal to 1, 2, 3, 4, or 5 print out invalid
+            } else {
+                switch (selectOption){
+                    case 1: 
+                        System.out.println("pressed 1");
+                        //System.out.println("test1");
+                        Customer newCustomer = createCustomer();
+                        Account newAccount = Account.createAccount();
+                        customerList.add(newCustomer);
+                        System.out.println("Account successfully created for " + newCustomer.Name + "!");
+                        break;
+                        
+                    case 2: 
+                        System.out.println("Enter the name of the customer you wish to open a new account for:"); 
+                        String customerSearch = keyboard.nextLine(); //Search for customer 
+                        Customer existingCustomer = new Customer(); //Copy existing customer so that all customer names ha
+                        for (Customer customer : customerList){ 
+                            if(customer.Name == customerSearch){
+                                existingCustomer.Name = customer.Name;
+                                existingCustomer.Address = customer.Address;
+                            }
+                        }
+                    
+                        Account existingCustomerNewAccount = Account.createAccount(); //Create a new account for existing customer
+                        existingCustomer.accountList.add(existingCustomerNewAccount); //add new account to existing customer 
+                        customerList.add(existingCustomer); //add customer to customer list
+                        break;
+                    
+                    case 3:
+                        System.out.println("test3");
+                        break;
+                        
+                    case 4: 
+                        System.out.println("test4");
+                        break;
+                        
+                    case 5:
+                        System.out.println("test5");
+                        break;
+                }
+            }
+            } else {
+                System.out.println("Input must be a number"); //if the input is not equal to a number print out invalid
+        }
+        
+        
+        //customerList.add(createCustomer()); //add new customer to the arraylist
+        //System.out.println(customerList.get(0).Name);          
+        //customerList.get(0).addCustomerAccount(Account.createAccount());
         
         try { 
             FileWriter myWriter = new FileWriter(myFile);
@@ -51,41 +138,6 @@ public class main
             System.out.println("There was an error writing to the file");
         }
         
-        try {
-            Scanner myReader = new Scanner(myFile);
-            ArrayList<String> fileNames = new ArrayList<String>();
-            
-            while(myReader.hasNextLine()){
-                myReader.useDelimiter(","); //seperates the informaion in the file by comma
-                 while (myReader.hasNext()) {
-                    fileNames.add(myReader.next());
-                }
-            }
-
-            //build customer
-            for (int i=0; i<fileNames.size(); i++){
-                System.out.println(fileNames.get(i));
-            }
-            
-            // Customer fileCustomer = new Customer(); 
-            // fileCustomer.Name = fileNames.get(0); //gets each element of the arrayList startring from 0
-            // fileCustomer.Address = fileNames.get(1);
-            
-            // Account fileAccount = new Account();
-            // fileAccount.accountNumber = fileNames.get(2);
-            // fileAccount.accountType = fileNames.get(3);
-            // Double accountBalance = Double.parseDouble(fileNames.get(4)); //converts the string to a double so that the maths in the account class works for the balance
-            // fileAccount.currentBalance = accountBalance;
-            
-            // fileCustomer.addCustomerAccount(fileAccount);
-            
-            // customerList.add(fileCustomer); //add the customer in the file to the customerList arraylist so that any existing customers can have their info updated 
-            
-        } catch(IOException e) {
-            System.out.println(e);
-            System.out.println("There was an error reading from the file");
-        }
-        
         while(running){
             
         }
@@ -104,51 +156,7 @@ public class main
         return customer1;
     }
     
-    public static Account createAccount(){ //this method is to create accounts with a number and type.
-        Scanner keyboard = new Scanner(System.in);
-                
-        System.out.println("Input account number");
-        Account account1 = new Account();  
-        //account1.accountNumber = keyboard.nextLine();
-        String accountNumberInput = keyboard.nextLine(); //User inputs account number
-        
-        System.out.println("Select an option for account type"); //Switch statement to select different account types
-        System.out.println("(1) - Everyday"); 
-        System.out.println("(2) - Savings"); 
-        System.out.println("(3) - Current");
-        
-        if(keyboard.hasNextInt()){
-            int choice = keyboard.nextInt(); //checks if the input is a number
-            keyboard.nextLine();
-            if (choice != 1 && choice != 2 && choice != 3){
-                System.out.println("Please enter a valid input"); //if the input is not equal to 1, 2, or 3, print out invalid
-            } else {
-                switch (choice){
-                    case 1: //Everyday   
-                        System.out.println("Everyday");
-                        EverydayAccount everyday = new EverydayAccount(); 
-                        account1 = everyday; //if "Everyday" is selected, account type is set to Everyday
-                        break;
-            
-                    case 2: //Savings
-                        System.out.println("Savings"); 
-                        SavingsAccount savings = new SavingsAccount();
-                        account1 = savings; //if "Savings" is selected, account type is set to Savings
-                        break;
-                
-                    case 3: //Current
-                        System.out.println("Current");
-                        CurrentAccount current = new CurrentAccount();
-                        account1 = current; //if "Current" is selected, account type is set to Current
-                        break;
-                }
-            }
-            } else {
-                System.out.println("Input must be a number"); //if the input is not equal to a number print out invalid
-        }
-        account1.accountNumber = accountNumberInput;
-        return account1;  
-    }
+    
 }
 
 
